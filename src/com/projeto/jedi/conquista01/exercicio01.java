bdb8f644a76961f70d07c3c67e4cbaa06bf79648
levyzera <levy.edu.11@gmail.com>
2022-01-29 22:16:02 -0300

    criado métdo main

---
+++ src/com/projeto/jedi/conquista01/exercicio01.java
@@ -0,0 +1,13 @@
+package com.projeto.jedi.conquista01;
+
+/**
+ * Declarar uma variável int numero dentro do escopo do método main e na linha seguinte atribuir um valor para ela. Após
+ * isso, usar o método System.out.println() para exibir o valor da variável no console. OBS*: Se atentar ao parâmetro do
+ * método de exibição
+ */
+public class exercicio01 {
+    public static void main(String[] args) {
+
+    }
+
+}
