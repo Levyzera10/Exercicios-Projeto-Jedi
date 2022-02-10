41f0456491462c147469c68106db0a2b5e047f0d
levyzera <levy.edu.11@gmail.com>
2022-02-09 22:05:02 -0300

    conquista02

---
+++ src/com/projeto/jedi/conquista02/exercicio02.java
@@ -0,0 +1,19 @@
+package conquista02;
+
+/**
+ * Criar um código que verifica se um valor de uma variável é igual a cem. Caso verdadeiro, escrever no console:
+ * “Verdadeiro”. Caso falso, escrever no console: “Falso”
+ */
+
+public class exercicio02 {
+    public static void main(String[] args) {
+        boolean verificacao = true;
+        int idade = 120;
+        if (idade == 100){
+            System.out.println(true);
+        }
+        else {
+            System.out.println("Falso");
+        }
+    }
+}
